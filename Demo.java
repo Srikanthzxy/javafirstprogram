62806f9c50b0a2071bf18a50f1afe39a85c2eb22
Srikanthzxy <139447689+Srikanthzxy@users.noreply.github.com>
2024-02-22 11:29:23 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,11 @@
+//to print HELLO, Hii,Hii Bye  using java
+import java.io.*;
+class Demo
+{
+     public static void main(String args[])
+    {
+         System.out.println("HELLO");
+         System.out.printf("Hii");
+         System.out.println("Hii"+" \t" + "Bye");
+     }
+}
